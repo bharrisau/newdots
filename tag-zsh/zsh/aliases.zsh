4c799eadb1e881cc44fb3be6a314cf3cb862cdda
Ben Harris <mail@bharr.is>
2014-12-22 12:03:52 +0800

    Added zsh

---
+++ tag-zsh/zsh/aliases.zsh
@@ -0,0 +1,43 @@
+#LS
+case "$(uname)" in
+    Linux)
+        alias ls='ls --color=auto' ;;
+    FreeBSD)
+        alias ls='ls -G' ;;
+esac
+
+alias l='ls -lFh' #size,show type,human readable
+alias la='ls -lAFh' #long list,show almost all,show type,human readable
+
+#EDITORS
+alias e='vim'
+
+#COLORFULNESS
+alias grep="grep --colour"
+
+#no vim to .vim corretion and expandig of aliases after sudo (tailing space)
+alias sudo='nocorrect sudo '
+
+#DIRS
+alias -g ...='../../' #cd ...
+alias -g ....='../../../' #cd ....
+alias -g .....='../../../../' #cd .....
+
+#sprunge paste service
+alias sprunge="curl -F 'sprunge=<-' http://sprunge.us"
+
+#common arch aliases
+alias p="yaourt"
+alias sc="systemctl"
+
+alias logs="sudo journalctl -f"
+
+alias uu="udiskie-umount"
+alias detach="udisks --detach"
+
+alias wifi="sudo wifi"
+alias bluetooth="sudo bluetooth"
+alias wwan="sudo wwan"
+
+alias suspend="slimlock & sc suspend"
+alias hibernate="slimlock & sc hibernate"
---
+++ tag-zsh/zsh/custom.zsh
@@ -0,0 +1,105 @@
+# Default editor
+EDITOR=vim
+export EDITOR
+
+# Default browser
+BROWSER=firefox
+export BROWSER
+
+# Ledger file
+LEDGER=~/ledger/personal.dat
+export LEDGER
+
+# Some aliases
+
+# ZMV for mass renaming
+autoload -Uz zmv
+
+# History options
+HISTFILE=~/.zsh_history
+HISTSIZE=1000
+SAVEHIST=1000
+setopt EXTENDED_HISTORY
+setopt INC_APPEND_HISTORY
+setopt HIST_IGNORE_ALL_DUPS
+setopt HIST_IGNORE_SPACE
+setopt HIST_REDUCE_BLANKS
+setopt HIST_VERIFY
+
+# Don't clobber files when redirecting stdout
+setopt NOCLOBBER
+setopt HIST_ALLOW_CLOBBER
+
+# Change word separator
+autoload -U select-word-style
+select-word-style bash
+
+# Set misc PATH variables
+if hash ruby &> /dev/null; then
+  PATH="$PATH:$(ruby -e 'puts Gem.user_dir')/bin"
+fi
+
+#export PATH
+# export MANPATH="/usr/local/man:$MANPATH"
+
+# # Preferred editor for local and remote sessions
+#if [[ -n $SSH_CONNECTION ]]; then
+# export EDITOR='vim'
+#else
+# export EDITOR='vim'
+#fi
+
+# Compilation flags
+# export ARCHFLAGS="-arch x86_64"
+
+# ssh
+# export SSH_KEY_PATH="~/.ssh/dsa_id"
+
+# ssh agent
+#if [ -f "${HOME}/.ssh-agent-info" ]; then
+#  . "${HOME}/.ssh-agent-info"
+#fi
+
+# added by travis gem
+[ -f /home/ben/.travis/travis.sh ] && source /home/ben/.travis/travis.sh
+
+# create a zkbd compatible hash;
+# to add other keys to this hash, see: man 5 terminfo
+typeset -A key
+
+key[Home]=${terminfo[khome]}
+
+key[End]=${terminfo[kend]}
+key[Insert]=${terminfo[kich1]}
+key[Delete]=${terminfo[kdch1]}
+key[Up]=${terminfo[kcuu1]}
+key[Down]=${terminfo[kcud1]}
+key[Left]=${terminfo[kcub1]}
+key[Right]=${terminfo[kcuf1]}
+key[PageUp]=${terminfo[kpp]}
+key[PageDown]=${terminfo[knp]}
+
+# setup key accordingly
+[[ -n "${key[Home]}"     ]]  && bindkey  "${key[Home]}"     beginning-of-line
+[[ -n "${key[End]}"      ]]  && bindkey  "${key[End]}"      end-of-line
+[[ -n "${key[Insert]}"   ]]  && bindkey  "${key[Insert]}"   overwrite-mode
+[[ -n "${key[Delete]}"   ]]  && bindkey  "${key[Delete]}"   delete-char
+[[ -n "${key[Up]}"       ]]  && bindkey  "${key[Up]}"       up-line-or-history
+[[ -n "${key[Down]}"     ]]  && bindkey  "${key[Down]}"     down-line-or-history
+[[ -n "${key[Left]}"     ]]  && bindkey  "${key[Left]}"     backward-char
+[[ -n "${key[Right]}"    ]]  && bindkey  "${key[Right]}"    forward-char
+[[ -n "${key[PageUp]}"   ]]  && bindkey  "${key[PageUp]}"   beginning-of-buffer-or-history
+[[ -n "${key[PageDown]}" ]]  && bindkey  "${key[PageDown]}" end-of-buffer-or-history
+
+# Finally, make sure the terminal is in application mode, when zle is
+# active. Only then are the values from $terminfo valid.
+if (( ${+terminfo[smkx]} )) && (( ${+terminfo[rmkx]} )); then
+    function zle-line-init () {
+        printf '%s' "${terminfo[smkx]}"
+    }
+    function zle-line-finish () {
+        printf '%s' "${terminfo[rmkx]}"
+    }
+    zle -N zle-line-init
+    zle -N zle-line-finish
+fi
