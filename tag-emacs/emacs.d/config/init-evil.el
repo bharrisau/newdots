9d6c02618a0d1d7e6832ef3f6d99c755de053dd6
Ben Harris <mail@bharr.is>
2014-12-23 08:22:26 +0800

    Updated Tue Dec 23 08:22:26 AWST 2014

---
+++ tag-emacs/emacs.d/config/init-evil.el
@@ -0,0 +1,92 @@
+(setq evil-search-module 'evil-search)
+(setq evil-magic 'very-magic)
+
+(setq evil-emacs-state-cursor '("red" box))
+(setq evil-normal-state-cursor '("green" box))
+(setq evil-visual-state-cursor '("orange" box))
+(setq evil-insert-state-cursor '("red" bar))
+(setq evil-replace-state-cursor '("red" bar))
+(setq evil-operator-state-cursor '("red" hollow))
+
+(setq evilnc-hotkey-comment-operator "gc")
+
+(require-package 'evil)
+(require-package 'evil-leader)
+(require-package 'evil-numbers)
+(require-package 'evil-visualstar)
+(require-package 'evil-nerd-commenter)
+(require-package 'evil-indent-textobject)
+(require-package 'evil-matchit)
+(require-package 'evil-exchange)
+(require-package 'evil-surround)
+
+(require 'evil)
+(require 'evil-nerd-commenter)
+(require 'evil-indent-textobject)
+(require 'evil-visualstar)
+
+(defcustom dotemacs-evil-state-modes
+  '(fundamental-mode
+    text-mode
+    prog-mode
+    sws-mode
+    compilation-mode)
+  "List of modes that should start up in Evil state."
+  :type '(repeat (symbol))
+  :group 'dotemacs)
+
+(defun my-enable-evil-mode ()
+  (if (apply 'derived-mode-p dotemacs-evil-state-modes)
+      (turn-on-evil-mode)
+    (set-cursor-color "red")))
+(add-hook 'after-change-major-mode-hook 'my-enable-evil-mode)
+
+(global-evil-leader-mode t)
+(global-evil-surround-mode t)
+(evil-exchange-install)
+
+(defun evilmi-customize-keybinding ()
+  (evil-define-key 'normal evil-matchit-mode-map
+    "%" 'evilmi-jump-items))
+(global-evil-matchit-mode t)
+
+(defun my-send-string-to-terminal (string)
+  (unless (display-graphic-p) (send-string-to-terminal string)))
+
+(defun my-evil-terminal-cursor-change ()
+  (when (string= (getenv "TERM_PROGRAM") "iTerm.app")
+    (add-hook 'evil-insert-state-entry-hook (lambda () (my-send-string-to-terminal "\e]50;CursorShape=1\x7")))
+    (add-hook 'evil-insert-state-exit-hook  (lambda () (my-send-string-to-terminal "\e]50;CursorShape=0\x7"))))
+  (when (and (getenv "TMUX") (string= (getenv "TERM_PROGRAM") "iTerm.app"))
+    (add-hook 'evil-insert-state-entry-hook (lambda () (my-send-string-to-terminal "\ePtmux;\e\e]50;CursorShape=1\x7\e\\")))
+    (add-hook 'evil-insert-state-exit-hook  (lambda () (my-send-string-to-terminal "\ePtmux;\e\e]50;CursorShape=0\x7\e\\")))))
+
+(add-hook 'after-make-frame-functions (lambda (frame) (my-evil-terminal-cursor-change)))
+(my-evil-terminal-cursor-change)
+
+(defun my-evil-modeline-change (default-color)
+  "changes the modeline color when the evil mode changes"
+  (let ((color (cond ((evil-insert-state-p) '("#002233" . "#ffffff"))
+                     ((evil-visual-state-p) '("#330022" . "#ffffff"))
+                     ((evil-normal-state-p) default-color)
+                     (t '("#440000" . "#ffffff")))))
+    (set-face-background 'mode-line (car color))
+    (set-face-foreground 'mode-line (cdr color))))
+
+(lexical-let ((default-color (cons (face-background 'mode-line)
+                                   (face-foreground 'mode-line))))
+  (add-hook 'post-command-hook (lambda () (my-evil-modeline-change default-color))))
+
+(defadvice evil-ex-search-next (after advice-for-evil-ex-search-next activate)
+  (evil-scroll-line-to-center (line-number-at-pos)))
+
+(defadvice evil-ex-search-previous (after advice-for-evil-ex-search-previous activate)
+  (evil-scroll-line-to-center (line-number-at-pos)))
+
+(defadvice evil-jump-forward (after advice-for-evil-jump-forward activate)
+  (evil-scroll-line-to-center (line-number-at-pos)))
+
+(defadvice evil-jump-backward (after advice-for-evil-jump-backward activate)
+  (evil-scroll-line-to-center (line-number-at-pos)))
+
+(provide 'init-evil)
